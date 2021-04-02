cabf0ef6607603b64909671daaf61f7d365ff011
Joaquinllopez00 <joaquinllopezzz@gmail.com>
2021-04-02 16:59:32 -0400

    Config

--- src/App.js
+++ src/App.js
@@ -4,30 +4,32 @@ import { Home } from "./pages/Home";
 import { Library } from "./components/Library";
 import { About } from "./components/About";
 import GlobalStyles from "./components/GlobalStyles";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 function App() {
   return (
     <div>
       <GlobalStyles />
       <Nav />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path={["/game/:id"]}>
-          <Home />
-        </Route>
-        <Route exact path="/library">
-          <Library />
-        </Route>
-        <Route path={["/library/game/:id"]}>
-          <Library />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-      </Switch>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path={["/game/:id"]}>
+            <Home />
+          </Route>
+          <Route exact path="/library">
+            <Library />
+          </Route>
+          <Route path={["/library/game/:id"]}>
+            <Library />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
