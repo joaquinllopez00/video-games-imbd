6c80ca322cb568cabdeb1b5b0682b526f5582341
Joaquinllopez00 <joaquinllopezzz@gmail.com>
2021-04-03 03:17:39 -0400

    config

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import pReducer from "./reducers/index.js";
 import { Provider } from "react-redux";
 import ReduxThunk from "redux-thunk";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/lib/integration/react";
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
